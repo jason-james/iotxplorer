1ee8f5af82f75a5ace04b7cf72c893d2419e8526
jason-james <jasongjames2012@hotmail.com>
2019-03-18 11:40:58 +0000

    production changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,30 @@
-const fs = require('fs');
-const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const webpack = require('webpack');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const glob = require('glob');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const fs = require("fs");
+const path = require("path");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require("webpack");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const glob = require("glob");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 const ANALYZE = false;
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: glob
-    .sync('./src/client/javascripts/*.js')
+    .sync("./src/client/javascripts/*.js")
     .reduce(
       (entries, entry) =>
-        Object.assign(entries, {[entry.replace('./src/client/javascripts/', '').replace('.js', '')]: entry}), {}
+        Object.assign(entries, {
+          [entry
+            .replace("./src/client/javascripts/", "")
+            .replace(".js", "")]: entry
+        }),
+      {}
     ),
   output: {
-    filename: '[name]-[chunkhash].js',
-    path: path.resolve(__dirname, 'dist/'),
+    filename: "[name]-[chunkhash].js",
+    path: path.resolve(__dirname, "dist/")
   },
   module: {
     rules: [
@@ -26,11 +32,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
-          options: JSON.parse(fs.readFileSync('./.babelrc')),
-        },
-      },
-    ],
+          loader: "babel-loader",
+          options: JSON.parse(fs.readFileSync("./.babelrc"))
+        }
+      }
+    ]
   },
   plugins: [
     ...(ANALYZE ? [new BundleAnalyzerPlugin()] : []),
@@ -38,21 +44,21 @@ module.exports = {
       cache: true,
       parallel: true,
       uglifyOptions: {
-        compress: false,
+        compress: {},
         ecma: 6,
         mangle: true,
-        comments: false,
+        comments: false
       },
       extractComments: true,
-      sourceMap: true,
+      sourceMap: true
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
     }),
     new ManifestPlugin({
-      basePath: '/',
-    }),
-  ],
+      basePath: "/"
+    })
+  ]
 };
