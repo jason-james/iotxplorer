4695292bb76ed6e54036c465cdc7a06c87c51068
jason-james <jasongjames2012@hotmail.com>
2019-03-18 12:10:32 +0000

    mobile fixes

--- src/shared/staking-dashboard/staking-dashboard.js
+++ src/shared/staking-dashboard/staking-dashboard.js
@@ -91,7 +91,7 @@ export class StakingDashboard extends Component {
 
     var responsiveOptions = [
       [
-        "screen and (min-width: 640px)",
+        "screen and (max-width: 640px)",
         {
           width: 400,
           height: 300
