7d01082b9425eee53cf3e66e2044ea140195100f
jason-james <jasongjames2012@hotmail.com>
2019-04-25 13:46:34 +0100

    USD value added

--- src/shared/address/address-container.js
+++ src/shared/address/address-container.js
@@ -1,13 +1,15 @@
-import {connect} from 'inferno-redux';
+import { connect } from "inferno-redux";
 
-import * as actions from '../address/address-actions';
-import {Address} from './address';
+import * as actions from "../address/address-actions";
+import fetchCoinPrice from "../common/nav/nav-actions";
+import { Address } from "./address";
 
 export const AddressContainer = connect(
   function mapStateToProps(state) {
     return {
       state: state.address,
       width: state.app.width,
+      price: state.nav.price
     };
   },
   dispatch => ({
@@ -21,6 +23,6 @@ export const AddressContainer = connect(
     fetchAddressCreateDepositsId: data =>
       dispatch(actions.fetchAddressCreateDepositsId(data)),
     fetchAccount: data => dispatch(actions.fetchAccount(data)),
-    fetchActionsByAddress: data => dispatch(actions.fetchActionsByAddress(data)),
+    fetchActionsByAddress: data => dispatch(actions.fetchActionsByAddress(data))
   })
 )(Address);
