9c46722a150b23ffd6b3fc0602830bb898a6244f
jason-james <jasongjames2012@hotmail.com>
2020-03-23 18:36:04 +0000

    update cmc api and database

--- src/server/gateways/coin-market-cap.js
+++ src/server/gateways/coin-market-cap.js
@@ -1,16 +1,20 @@
-import axios from 'axios';
+import axios from "axios";
 
 export class CoinMarketCap {
   fetchCoinPrice() {
     // there are usd, btc and eth prices
-    const url = 'https://api.coinmarketcap.com/v1/ticker/iotex/?convert=ETH';
+    const url = "https://api.coinmarketcap.com/v1/ticker/iotex/?convert=ETH";
     return axios.get(url);
   }
 
   fetchMarketData() {
     // usd prices
-    const url = 'https://api.coinmarketcap.com/v1/ticker/iotex/';
-    return axios.get(url);
+    const url =
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=IOTX";
+    return axios.get(url, {
+      headers: {
+        "X-CMC_PRO_API_KEY": "24870ad1-be93-44c3-8a8c-18bf07e72fb0"
+      }
+    });
   }
-
 }
--- src/shared/blockchain-explorer/market-dashboard-handler.js
+++ src/shared/blockchain-explorer/market-dashboard-handler.js
@@ -1,34 +1,34 @@
-import {DASHBOARD} from '../common/site-url';
+import { DASHBOARD } from "../common/site-url";
 
 export function setMarketDashboardRoutes(server) {
   const {
-    gateways: {coinmarketcap},
+    gateways: { coinmarketcap }
   } = server;
 
   async function getMarketData(ctx, next) {
     try {
       const response = await coinmarketcap.fetchMarketData();
-      const d = response.data[0];
+      const d = response.data.data.IOTX;
       const marketData = {
-        marketCap: new Intl.NumberFormat('en-US', {
-          maximumFractionDigits: 1,
-        }).format(d.market_cap_usd),
-        supply: new Intl.NumberFormat('en-US', {
-          maximumFractionDigits: 0,
-        }).format(d.available_supply),
-        volume: new Intl.NumberFormat('en-US', {
-          maximumFractionDigits: 0,
-        }).format(d['24h_volume_usd']),
+        marketCap: new Intl.NumberFormat("en-US", {
+          maximumFractionDigits: 1
+        }).format(d.quote.USD.market_cap),
+        supply: new Intl.NumberFormat("en-US", {
+          maximumFractionDigits: 0
+        }).format(d.circulating_supply),
+        volume: new Intl.NumberFormat("en-US", {
+          maximumFractionDigits: 0
+        }).format(d.quote.USD.volume_24h)
       };
 
-      ctx.body = {ok: true, marketData};
+      ctx.body = { ok: true, marketData };
     } catch (error) {
       ctx.body = {
         ok: false,
-        error: {code: 'FAIL_GET_MARKET_DATA', message: 'error.unknown'},
+        error: { code: "FAIL_GET_MARKET_DATA", message: error }
       };
     }
   }
 
-  server.post('getMarketData', DASHBOARD.MARKET_DATA, getMarketData);
+  server.post("getMarketData", DASHBOARD.MARKET_DATA, getMarketData);
 }
